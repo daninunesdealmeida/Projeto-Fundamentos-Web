68cbd8e6571914d0f58f16eea5d98c5dae67634c
Daniel Nunes de Almeida <daninunesdealmeida@gmail.com>
2020-04-07 19:29:03 -0300

    main commit - full course

---
+++ Canvas.js
@@ -0,0 +1,50 @@
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+
+function InitThis() {
+    ctx = document.getElementById('myCanvas').getContext("2d");
+
+    // funções para movimentar o mouse
+    $('#myCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#myCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#myCanvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+	    $('#myCanvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    // função para realizar o desenho
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+	
+function clearArea() {
+    // função para limpar a tela
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+
+
---
+++ js.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#fone").mask("(99)99999-9999");
+});
+
+//const email_validator = document.getElementById("email").value;
+
+// function validar() {
+//     if (document.getElementById("nome").value.length < 3) {
+//         document.getElementById("nome").focus();
+//         return false;
+//     }
+
+//     if (document.getElementById("telefone").value.length < 3) {
+//         document.getElementById("telefone").focus();
+//         return false;
+//     }
+
+//     if (document.getElementById("email").value.length < 3) {
+//         document.getElementById("email").focus();
+//         return false;
+//     }
+
+//     if (document.getElementById("datan").value.length < 3) {
+//         document.getElementById("datan").focus();
+//         return false;
+//     }
+
+//     return true;
+// }
+
+
+// if (email_validator.endswith("upf.br")) {
+//     //return validar();
+//     console.log(validar)
+// } else {
+//     return alert ('Email inválido');
+// }
